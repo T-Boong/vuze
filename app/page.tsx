b3974f1b1b302c2fa53bd92e4dd001d4fb202b23
jinha_yoo <jinha_yoo@tmax.co.kr>
2025-04-08 15:45:51 +0900

    refactor : main page 리다이렉션

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
+import MainPage from "./main/page";
 
 export default function Home() {
-  return (
-    <div className="w-full h-[calc(100vh-60px)]  flex justify-center items-center ">
-      <button className="p-2 rounded-md text-white">
-        <Link href="/auth">시작하기</Link>
-      </button>
-    </div>
-  );
+  return <MainPage />;
 }
