a136d04ca03d5425602abde36b862551296bbaae
jinha_yoo <jinha_yoo@tmax.co.kr>
2025-04-09 12:58:51 +0900

    fix : type error

--- types/youtube/channel.ts
+++ types/youtube/channel.ts
@@ -1,18 +1,38 @@
 export interface ChannelDetail {
   id: string;
+  kind?: string;
+  etag: string;
   snippet: {
     title: string;
+    description: string;
+    customUrl: string;
+    publishedAt: string;
     thumbnails: {
-      default: { url: string };
-      medium: { url: string };
-      high: { url: string };
+      default: { url: string; width: number; height: number };
+      medium: { url: string; width: number; height: number };
+      high: { url: string; width: number; height: number };
     };
+    defaultLanguage?: string;
+    localized: {
+      title: string;
+      description: string;
+    };
+    country?: string;
   };
   statistics: {
+    viewCount: string;
     subscriberCount: string;
+    hiddenSubscriberCount: boolean;
+    videoCount: string;
   };
 }
 
 export interface YoutubeChannelsResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
   items: ChannelDetail[];
 }
